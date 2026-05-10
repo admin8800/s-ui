f63ec0db18b709d14fe0eda29aa40bb8f31df5d1
admin8800 <admin8800admin8800@gmail.com>
2026-05-10 16:12:39 +0800

    更改默认语言和时区

--- service/setting.go
+++ service/setting.go
@@ -52,7 +52,7 @@ var defaultValueMap = map[string]string{
 	"webURI":        "",
 	"sessionMaxAge": "0",
 	"trafficAge":    "30",
-	"timeLocation":  "Asia/Tehran",
+	"timeLocation":  "Asia/Shanghai",
 	"subListen":     "",
 	"subPort":       "2096",
 	"subPath":       "/sub/",
